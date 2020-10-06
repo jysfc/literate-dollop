08085cdbb13b5300eb7734b68e250d46d6a55519
jysfc <63255178+jysfc@users.noreply.github.com>
2020-10-06 13:45:52 -0700

    created edit property page

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ $("#sign-up-button").click(function () {
    $("#create-account-card").toggleClass("d-none");
    $("#sign-up-button").toggleClass("d-none");
 });
+
+//edit button
+$("#edit-button").click(function () {
+   $("#edit-property").toggleClass("d-none");
+   $("#edit-button").toggleClass("d-none");
+});
