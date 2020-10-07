5bf5014d4696323559531f2eeb12c56783ab7922
jysfc <63255178+jysfc@users.noreply.github.com>
2020-10-07 13:58:31 -0700

    added checkbox title

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/*
-// overlay
-$("#save-card").click(function () {
-   $("#overlay-success").toggleClass("d-flex d-none");
-});
-
-$("#back-to-answer").click(function () {
-   $("#overlay-error").toggleClass("d-flex d-none");
-});
-
-//show-delete checkbox
-$("#show-delete").click(function () {
-   $("#delete-button").removeClass("d-none");
-});
-*/
-
 //sign up button
 $("#sign-up-button").click(function () {
    $("#create-account-card").toggleClass("d-none");
